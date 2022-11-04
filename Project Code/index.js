e9f9f5e4679a067f06a30dbf2a93083f44d3df95
nibo6686 <nibo6686@colorado.edu>
2022-11-04 12:54:18 -0600

    register page created

--- Project Code/index.js
+++ Project Code/index.js
@@ -8,11 +8,12 @@ app.post('/register', async (req, res) => {
         res.redirect('register');
     }
     const hash = await bcrypt.hash(req.body.password, 10);
-    const query = 'insert into users (first_name, last_name, email, password) values ($1, $2, $3, $4);';
+    const query = 'insert into users (first_name, last_name, email, username, password) values ($1, $2, $3, $4, $5);';
     db.any(query, [
         req.body.first_name,
         req.body.last_name,
         req.body.email,
+        req.body.username,
         hash
     ])
     .then(function (data) {
