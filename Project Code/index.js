282cbcb909e03938422278267528302babad8ca8
rera8642 <rera8642@colorado.edu>
2022-11-08 17:28:24 -0700

    Setup index.js page, working with docker compose

--- Project Code/index.js
+++ Project Code/index.js
@@ -1,3 +1,52 @@
+const express = require('express');
+const app = express();
+const pgp = require('pg-promise')();
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const axios = require('axios');
+const pgPromise = require('pg-promise');
+
+const dbConfig = {
+    host: 'db',
+    port: 5432,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+};
+
+const db = pgp(dbConfig);
+
+db.connect()
+    .then(obj => {
+        console.log('Database connection successful');
+        obj.done();
+    })
+    .catch(error => {
+        console.log('ERROR:', error.message || error)
+    });
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        saveUninitialized: false,
+        resave: false,
+    })
+);
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+app.get('/', (req,res) => {
+    res.redirect('/login');
+});
+
 app.get('/register', (req, res) => {
     res.render('pages/register');
 });
@@ -24,4 +73,16 @@ app.post('/register', async (req, res) => {
         res.redirect('register');
     });
 });
-  
+
+const auth = (req, res, next) => {
+    if (!req.session.user) {
+        req.session.message = "Please Register";
+        return res.redirect('/register');
+    }
+    next();
+};
+
+app.use(auth);
+
+app.listen(3000);
+console.log('Server is listening on port 3000');
